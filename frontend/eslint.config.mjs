c8408b39ebad4caadf7fdb3c239c7edac9056993
Mary Mettille <mettillemarya@gmail.com>
2024-09-18 14:49:41 -0500

    Add ESLint and Prettier for the frontend - code standards matter

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,26 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import pluginPrettier from 'esint-plugin-prettier';
+
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      globals: globals.browser,
+      parser: tseslint.parser,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    plugins: {
+      prettier: pluginPrettier,
+    },
+  },
+];
