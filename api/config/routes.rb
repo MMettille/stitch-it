7cf5983fee43f6784b8914d83e642a71dee4bfa7
Mary Mettille <mettillemarya@gmail.com>
2024-09-25 20:09:02 -0500

    Ditch the redundant api folder

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api, constraints: { format: 'json' } do
-    namespace :v1 do
-      resources :patterns, only: %i[create index update destroy]
-    end
+  namespace :v1, constraints: { format: 'json' } do
+    resources :patterns, only: %i[create index update destroy]
   end
 end
